1d651b323e79e5af3887cf2d7947ec2b47181a2f
Edward Brash <brash@jlab.org>
2026-05-10 19:36:40 -0400

    Add Week 9-14 labs

---
+++ labs/lab13_pandas_dataframe_analysis/starter_code/weather_analysis.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def load_weather_data(filename):
+    pass
+
+
+def print_summary(df):
+    pass
+
+
+def add_temperature_range(df):
+    pass
+
+
+def plot_temperatures(df):
+    pass
+
+
+def main():
+    filename = "../data/weather_june.csv"
+    pass
+
+
+main()
---
+++ labs/lab14_curve_fitting_regression/starter_code/regression_analysis.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_data(filename):
+    pass
+
+
+def fit_line(x, y):
+    pass
+
+
+def predict(x, slope, intercept):
+    pass
+
+
+def main():
+    filename = "../data/study_scores.csv"
+    pass
+
+
+main()
