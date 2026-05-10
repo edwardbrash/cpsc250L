16e5365be010b5d9fd68d971b73b19c5f640376f
Edward Brash <brash@jlab.org>
2026-05-10 10:05:06 -0400

    Add Week 2 object-oriented programming labs

---
+++ labs/lab08_oop_review_challenge/starter_code/book.py
@@ -0,0 +1,36 @@
+# Lab 8: OOP Review Challenge
+
+
+class Book:
+    def __init__(self, title, author, year, price, quantity):
+        pass
+
+    def inventory_value(self):
+        """
+        Return price * quantity.
+        """
+        pass
+
+    def add_stock(self, amount):
+        """
+        Increase quantity by amount.
+        """
+        pass
+
+    def sell_copies(self, amount):
+        """
+        Decrease quantity by amount if enough copies are available.
+
+        Return True if the sale succeeds.
+        Return False otherwise.
+        """
+        pass
+
+    def __str__(self):
+        pass
+
+    def __lt__(self, other):
+        """
+        Compare books alphabetically by title.
+        """
+        pass
---
+++ labs/lab08_oop_review_challenge/starter_code/bookstore_inventory.py
@@ -0,0 +1,79 @@
+# Lab 8: Object-Oriented Programming Review Challenge
+
+from book import Book
+
+
+def create_inventory():
+    """
+    Create and return a list of Book objects.
+    """
+    books = []
+
+    # TODO: Add at least five books to the list.
+
+    return books
+
+
+def print_inventory(books):
+    """
+    Print every book in the inventory.
+    """
+    pass
+
+
+def total_inventory_value(books):
+    """
+    Return the total value of all books in inventory.
+    """
+    pass
+
+
+def find_by_author(books, author):
+    """
+    Return a list of books written by the specified author.
+    """
+    pass
+
+
+def find_low_stock(books, threshold):
+    """
+    Return a list of books whose quantity is less than or equal to threshold.
+    """
+    pass
+
+
+def print_books(books):
+    """
+    Print a list of books.
+    """
+    pass
+
+
+def main():
+    inventory = create_inventory()
+
+    print("Full Inventory")
+    print("--------------")
+    print_inventory(inventory)
+
+    print()
+    print("Total inventory value:", total_inventory_value(inventory))
+
+    print()
+    print("Books by Octavia Butler")
+    print("-----------------------")
+    print_books(find_by_author(inventory, "Octavia Butler"))
+
+    print()
+    print("Low Stock Books")
+    print("---------------")
+    print_books(find_low_stock(inventory, 3))
+
+    print()
+    print("Sorted by Title")
+    print("---------------")
+    sorted_books = sorted(inventory)
+    print_books(sorted_books)
+
+
+main()
