7d0cde51e0b8e1422a055af21920f2f069eef3fc
Edward Brash <brash@jlab.org>
2026-06-08 11:40:55 -0400

    complete program

--- labs/lab10_recursion_and_timing/starter_code/fibonacci_timing.py
+++ labs/lab10_recursion_and_timing/starter_code/fibonacci_timing.py
@@ -2,26 +2,42 @@ import time
 
 
 def fib_recursive(n):
-    pass
+    if n <= 1:
+        return n
+    else:
+        return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 
 def fib_iterative(n):
-    pass
+    if n <= 1:
+        return n
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
 
 
 def time_function(function, n):
-    pass
+    start_time = time.time()
+    function(n)
+    end_time = time.time()
+    return end_time - start_time
 
 
 def main():
-    values = [5, 10, 20, 25, 30]
+    values = [5, 10, 20, 25, 30, 35, 40]
 
     print("Fibonacci Timing")
     print("----------------")
-    print("n    recursive_time    iterative_time")
+    print("n    recursive_time    iterative_time        speed factor")
 
     for n in values:
-        pass
+        recursive_time = time_function(fib_recursive, n)
+        iterative_time = time_function(fib_iterative, n)
+        if iterative_time !=0:
+            speed = recursive_time / iterative_time
+        print(f"{n:<5} {recursive_time:.8f} seconds    {iterative_time:.8f} seconds     {speed:.1f}")
 
 
 main()
