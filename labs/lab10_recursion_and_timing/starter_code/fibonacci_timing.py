bbb4243263acea9f2df606d8b869d58a32aa65dc
poejayden60 <poejayden60@gmail.com>
2026-06-16 13:53:55 -0400

    Completed Assignment

--- labs/lab10_recursion_and_timing/starter_code/fibonacci_timing.py
+++ labs/lab10_recursion_and_timing/starter_code/fibonacci_timing.py
@@ -1,20 +1,31 @@
 import time
 
-def fib_recursive(n)
-    # TODO: write this function
-    pass
+import matplotlib.pyplot as plt
 
+def fib_recursive(n):
+    if n <= 1:
+        return n
+    return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 def fib_iterative(n):
     # TODO: write this function
-    pass
+    if n <= 1:
+        return n
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
 
+    return b
 
 def time_function(function, n):
     # TODO: write this function - google the python time module to figure out how it works
     # TODO: start a timer, call the appropriate function, then stop the timer
     # TODO: return the elapsed time
-    return 0
+    start = time.perf_counter()
+    function(n)
+    end = time.perf_counter()
+
+    return end - start
 
 def main():
     values = [5, 10, 20, 25, 30, 35, 40]
@@ -22,15 +33,28 @@ def main():
     print("Fibonacci Timing")
     print("----------------")
     print("n    recursive_time    iterative_time")
-
+    rectime = []
+    inttime = []
     for n in values:
         recursive_time = time_function(fib_recursive, n)
         iterative_time = time_function(fib_iterative, n)
+        rectime.append(recursive_time)
+        inttime.append(iterative_time)
         if iterative_time != 0:
             speed = recursive_time/iterative_time
         else:
             speed = float("inf")
         print(f"{n:<5} {recursive_time:.8f} seconds    {iterative_time:.8f} seconds     {speed:.1f}")
+    plt.plot(values,rectime, label = "Recursive Time")
+    plt.plot(values,inttime, label = "Iterative Time")
+    plt.xlabel("n")
+    plt.ylabel("Time")
+    plt.title("Recursive Time and Iterative Time")
+    plt.legend()
+    plt.yscale("log")
+    plt.show()
+
+
 
     # TODO: create a plot which shows both recursive time and iterative time as a function of n
     # TODO: label the x-axis, y-axis, and provide a title
