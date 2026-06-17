b89c1946ace826672c6d17db7dcadd16e135dd98
Edward Brash <brash@jlab.org>
2026-06-17 12:06:03 -0400

    Recover Jayden's submitted Lab 3 work

--- labs/lab03_functions_modular_design/starter_code/temperature_report.py
+++ labs/lab03_functions_modular_design/starter_code/temperature_report.py
@@ -1,20 +1,46 @@
-def read_temperatures(filename):
-    pass
+def read_temperatures(values):
+   #Opens file with a call "filename" and returns the temperature date from the txt file and skips over blanks.
+    temperatures = []
+    filename = "../data/june_temperatures.txt"
+    with open(filename, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line != "":
+                temperatures.append(int(line))
+    return temperatures
+
 
 def calculate_average(values):
-    pass
+  #a value takes the length of the temperatures and sum is getting the some of all the temperature values and adding them and average is dividing that number by how many values there are.
+    a = len(values)
+    sum = 0.0
+    for i in range(a):
+        sum = sum + values[i]
+    average = sum / a
+    return average
 
 def find_maximum(values):
-    pass
+    #this will automatically give the highest of temperatures
+    return max(values)
 
 def find_minimum(values):
-    pass
+    # this will automatically give the highest of temperatures
+    return min(values)
 
 def count_above_threshold(values, threshold):
-    pass
+    count = 0
+    for i in values:
+        if i > threshold:
+            count = count + 1
+    return count
 
 def print_report(values):
-    pass
+    print("Temperature Report")
+    print("------------------")
+    print(f'Average temperature: {calculate_average(values)}')
+    print(f'Maximum temperature: {find_maximum(values)}')
+    print(f'Minimum temperature: {find_minimum(values)}')
+    print(f'Temperatures above 80: {count_above_threshold(values, 80)}')
 
 def main():
     temperatures = read_temperatures("../data/june_temperatures.txt")
